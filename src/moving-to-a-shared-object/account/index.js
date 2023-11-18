d32e9b42d672b991d7a6a3ddafb363f26611e4ae
Kaio Silveira <silveira.kaio@icloud.com>
2023-11-18 18:36:40 +0000

    moving-to-a-shared-object: introduce assertion to make sure interest rates match

--- src/moving-to-a-shared-object/account/index.js
+++ src/moving-to-a-shared-object/account/index.js
@@ -1,7 +1,10 @@
+import assert from 'assert';
+
 export class Account {
   constructor(number, type, interestRate) {
     this._number = number;
     this._type = type;
+    assert(interestRate === this._type.interestRate);
     this._interestRate = interestRate;
   }
 
--- src/moving-to-a-shared-object/account/index.test.js
+++ src/moving-to-a-shared-object/account/index.test.js
@@ -3,7 +3,7 @@ import { AccountType } from '../account-type';
 
 describe('Account', () => {
   it('should have an interest rate', () => {
-    const normalAccount = new AccountType('normal');
+    const normalAccount = new AccountType('normal', 0.03);
     const account = new Account('1234', normalAccount, 0.03);
     expect(account.interestRate).toEqual(0.03);
   });
